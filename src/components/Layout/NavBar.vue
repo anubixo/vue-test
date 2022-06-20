71ac2d07597ac97d30a08d7b6b6ed0e7566041b6
AhmedHani <ahmedalmoselhy.slm@gmail.com>
2022-06-20 15:43:07 +0200

    change NavBar component path

---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,84 @@
+<script>
+import Currencies from "./Partials/Currencies.vue";
+import Languages from "./Partials/Languages.vue";
+export default {
+  data() {
+    return {
+      currencies: [
+        {
+          name: "Euro",
+          value: "EUR",
+        },
+        {
+          name: "Dollar",
+          value: "USD",
+        },
+        {
+          name: "Egyptian Pound",
+          value: "EGP",
+        },
+      ],
+      languages: [
+        {
+          name: "Arabic",
+          value: "ar",
+          flag: "./img/ksa.png",
+        },
+        {
+          name: "English",
+          value: "en",
+          flag: "./img/usa.png",
+        },
+        {
+          name: "Spanish",
+          value: "es",
+          flag: "./img/esp.png",
+        },
+        {
+          name: "French",
+          value: "fr",
+          flag: "./img/fra.png",
+        },
+        
+      ],
+      flag: String,
+    }
+  },
+  components: { Currencies, Languages }
+}
+</script>
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand" href="home.html"><img src="../../../img/travveo-logo.svg" alt=""></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <Currencies :currencies="currencies" />
+          <li class="nav-item">
+            <a class="nav-link" href="profile.html">
+              <img src="../../../img/profile.png" alt="" width="32px" height="32px">
+            </a>
+          </li>
+          <Languages :languages="languages" flag="./img/usa.png"/>
+          <li class="nav-item ">
+            <a class="nav-link sign-up text-center mt-2" href="list-property.html">list your property</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link btn btn-secondary dropdown-toggle sign-up mt-2" type="button" id="dropdownMenuButton"
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Register
+            </a>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="register.html">Register</a>
+              <a class="dropdown-item" href="sign-in.html">Sign in</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
